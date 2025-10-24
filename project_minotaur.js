082022f03552f2792e1eb22df7d345e222a27836
MichaelRLB <159464342+MichaelRLB@users.noreply.github.com>
2025-10-23 21:37:04 -0400

    base js and q&a code started

---
+++ project_minotaur.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById('game-canvas'),
+    context = canvas.getContext('2d')
+
+//check snailBait chapter 1 for code
+
+// starting positions
+    //PLAYER_START_LEFT
+    //PLAYER_START_UP(?)
+
+// Images
+
+    //background = new Image(),
+    //playerImage = new Image(),
+
+    // Level code
+
+// Launch game
+
--- text_box.js
+++ text_box.js
@@ -7,6 +7,9 @@
 var talkActive = false;
 var textStage = 0;
 const TextBox = document.getElementById('textBox');
+const Choices = document.getElementById('answers');
+
+const riddleAnswers = ["False", "False", "True", "Falsetto"];
 
 //interact function; change this so that it just passes inputs to dialogue box later (check notebook)
 
@@ -14,25 +17,61 @@ function interact(){
     if(talkActive === false){
         talkActive = true;
         TextBox.classList.add('fadeIn');
-        window.dialogue();
+        //window.dialogue();
+        window.riddleTest();
     }
-    else{
+    else{ //add an else if; if the riddle answers are on screen (aka "textStage === 2" / "buttonVisibility = on") and the player hits "e", textStage will go back one so you can read the riddle again (and stop players from skipping it).
         textStage += 1;
-        window.dialogue();
+        //window.dialogue();
+        window.riddleTest();
     }
 }
+function riddleTest(){ 
+    var playerChoice;
+    //console.log('textStage = ' + textStage);
+    if(textStage === 0){
+        document.getElementById('dialogueText').innerHTML = "Riddle me this:";
+    }
+    if(textStage === 1){
+        document.getElementById('dialogueText').innerHTML = "I am answer number three.";
+    } 
+    if(textStage === 2){ 
+        document.getElementById('dialogueText').innerHTML = "What am I?";
+        Choices.classList.add('activate');
+        //pass riddleAnswers to the buttons
+        document.getElementById('choice1').innerHTML = riddleAnswers[0];
+        document.getElementById('choice2').innerHTML = riddleAnswers[1];
+        document.getElementById('choice3').innerHTML = riddleAnswers[2];
+        document.getElementById('choice4').innerHTML = riddleAnswers[3];
+    } 
+    if(textStage === 3){ //change this to an if-else statement based on whether the player is right or wrong.
+        Choices.classList.remove('activate');
+        document.getElementById('dialogueText').innerHTML = "This is where you're told if you're right or wrong; after this, the dialogue box will close.";
+    } 
+    //close dialogue, reset talkActive and textStage values.
+    else if (textStage === 4){
+        document.getElementById('dialogueText').innerHTML = "ok bye lol.";      
+        TextBox.classList.remove('fadeIn');
+        talkActive = false;
+        textStage = 0;
+    }
+}
+
 function dialogue(){ //look into making an array for dialogue lines to cut down on else if branches.
-    console.log('textStage = ' + textStage);
+    //console.log('textStage = ' + textStage);
     if(textStage === 0){
         document.getElementById('dialogueText').innerHTML = "This is where the first line of dialogue goes; press E to advance.";
     }
     if(textStage === 1){
         document.getElementById('dialogueText').innerHTML = "This is where the riddle will go in the final game.";
     } 
-    if(textStage === 2){
+    if(textStage === 2){ 
         document.getElementById('dialogueText').innerHTML = "This is where you will be asked to answer the riddle; here, four buttons will pop up and you must pick the right answer with the mouse.";
+        Choices.classList.add('activate');
+        
     } 
     if(textStage === 3){
+        Choices.classList.remove('activate');
         document.getElementById('dialogueText').innerHTML = "This is where you're told if you're right or wrong; after this, the dialogue box will close.";
     } 
     //close dialogue.
@@ -44,7 +83,7 @@ function dialogue(){ //look into making an array for dialogue lines to cut down 
     }
 }
 
-//makes the text box fade in and out when you press "e"; refactor this into dialogue system later
+//makes the text box fade in and out when you press "e";
 /*function fadeInTextBox() {
     //console.log('fadeIn function active')
     var testDialogue;
@@ -62,13 +101,12 @@ function dialogue(){ //look into making an array for dialogue lines to cut down 
 
 //event listeners / controls
 
-window.addEventListener('keydown', function (e){
+window.addEventListener('keyup', function (e){
     var key = e.keyCode;
 
     //if talkActive is false, opens dialogue box. If true, it advances dialogue, eventually closing the box.
     if (key === 69){ // 'e' key
         //console.log('input received = interact');
-        //change this to "interact" function when ready.
         window.interact();
     }
 })
